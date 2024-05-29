23c5690b24da276c52f94447038d635098195a1f
UselessWeirdUser <uselessweirduser@gmail.com>
2024-05-29 14:49:36 +0200

    initial commit

---
+++ src/main/java/it/feargames/killcounter/KillCounter.java
@@ -0,0 +1,17 @@
+package it.feargames.killcounter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KillCounter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
